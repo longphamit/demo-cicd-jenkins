4b7ac145850219257f35945fbffa6ab759f3c3bf
phamlong992k@gmail.com <phamlong992k@gmail.com>
2022-02-22 11:59:29 +0700

    init project

---
+++ src/main/java/com/longpc/democicdjenkins/DemoCicdJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.longpc.democicdjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCicdJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCicdJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/longpc/democicdjenkins/DemoCicdJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longpc.democicdjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCicdJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
